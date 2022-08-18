9f7af79f0084baf4e3a54be1798d642f40f30d16
Alex Johnson <johnsonav1992@gmail.com>
2022-08-18 11:04:01 -0500

    rollbar add

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,7 +1,19 @@
 const path = require('path')
+require('dotenv').config()
+
+// include and initialize the rollbar library with your access token
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello World!')
 
 module.exports = {
     getHomePage: (req, res) => {
+        rollbar.log('They hit our website')
         res.sendFile(path.join(__dirname, '../../client/index.html'))
     },
 
@@ -10,6 +22,7 @@ module.exports = {
     },
 
     getJS: (req, res) => {
+        rollbar.log('got the JS')
         res.sendFile(path.join(__dirname, '../../client/main.js'))
     }
 }
